67ceee9bc8f297c3fcc1dbef944d1b658d1ae9b9
MoazzemhossainMoazz <moazzemh222@gmail.com>
2026-05-23 23:26:38 +0600

    Added EmployeeOOP

---
+++ EmployeeOOP/EmployeeOOP/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOOP
+{
+    internal class Employee
+    {
+        static int idCounter = 0;
+        public Employee()
+        {
+            Id = ++idCounter;
+        }
+
+        
+        public enum Grade
+        {
+            Junior = 1,
+            Mid,
+            Senior,
+            Lead,
+            Manager
+        }
+
+        public int Id { get; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime JoinDate { get; set; }
+        public Grade GradeName { get; set; } 
+        public string RolePlays { get; set; }
+    }
+}
---
+++ EmployeeOOP/EmployeeOOP/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> Employeelist = new List<Employee>();
+            Employee employee1 = new Employee()
+            {
+                Name = "Sakib",
+                DOB = new DateTime(1992, 3, 2),
+                JoinDate = new DateTime(2020, 1, 2),
+                GradeName=Employee.Grade.Junior,
+                RolePlays = "Developer"
+            };
+
+            Employee employee2 = new Employee() 
+            { Name = "Tamim", DOB = new DateTime(1993, 4, 3), JoinDate = new DateTime(2023, 1, 3), GradeName = Employee.Grade.Senior, RolePlays ="Developer" 
+            };
+            Employee employee3 = new Employee() 
+            { Name = "Mushfiq", DOB = new DateTime(1991, 4, 3), JoinDate = new DateTime(2023, 3, 3), GradeName = Employee.Grade.Lead, RolePlays = "HR" 
+            };
+
+            Employeelist.Add(employee1);
+            Employeelist.Add(employee2);
+            Employeelist.Add(employee3);
+
+            Console.WriteLine($"ID\tName\tDOB\tJoinDate\tGrade\tRolePlays");
+            foreach (var employee in Employeelist)
+            {
+                Console.WriteLine(employee.Id + "\t" + employee.Name + "\t" + employee.DOB.ToShortDateString() + "\t" + employee.JoinDate.ToShortDateString() + "\t" + employee.GradeName + "\t" + employee.RolePlays);
+            }
+            
+
+        }
+    }
+}
